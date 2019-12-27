cecf7708bf70c27e0d0bd886936c5b48db87cc55
surajkarki66 <suraj.karki500@gmail.com>
2019-12-27 22:39:48 +0545

    Final Commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -63,20 +63,7 @@ $(document).ready(function() {
           offset: '50%'
       });
 
-      /* Mobile navigation */
-    $('.js--nav-icon').click(function() {
-        var nav = $('.js--main-nav');
-        var icon = $('.js--nav-icon i');
-        
-        nav.slideToggle(200);
-        
-        if (icon.hasClass('ion-navicon-round')) {
-            icon.addClass('ion-close-round');
-            icon.removeClass('ion-navicon-round');
-        } else {
-            icon.addClass('ion-navicon-round');
-            icon.removeClass('ion-close-round');
-        }        
-    });
-      
+
+
 });
+
